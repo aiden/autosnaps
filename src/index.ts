c119abbd76dd9a8264cdf0f34286f4643f99372d
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2018-10-13 08:49:58 +0400

    Add a simple withSnapshot function

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
-/**
- * Sums 2 numbers
- */
-export function sum(a: number, b: number) {
-  return a + b;
+export function withSnapshot<T extends Function>(fn: T): T {
+  // It seems impossible to type this cleanly so we typecast (func -> any -> T) so we don't ruin the users' types.
+  // Luckily we aren't really doing any logic in there that has anything to do with the input or
+  // output, so we don't really need the static typing.
+  return (function(...args: any[]) {
+    return fn(...args);
+  } as any) as T;
+}
+
+function recordFixture(args: any[]): void {
+  console.log(JSON.stringify(args));
 }
